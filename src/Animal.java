6c7d3c5041b2f6db19ac7bf5c3361ff04fb5c4ac
kirAlx <kiralx@bk.ru>
2023-07-23 11:25:39 +0300

    added abstract class and interface

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public abstract class Animal {
+    private String name;
+    private Integer birthDate;
+    private List<String> commands;
+}
---
+++ src/Repository.java
@@ -0,0 +1,6 @@
+public interface Repository {
+    void add(Animal animal);
+    void printAllAnimals();
+    Animal removeAnimal(Integer i);
+
+}
